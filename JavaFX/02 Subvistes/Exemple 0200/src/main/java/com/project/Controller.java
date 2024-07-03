b23d25d51cde793e12ff0dd59fdd91599c4aa7b1
optimisme <optimisme@gmail.com>
2024-07-03 15:20:54 +0200

    .

--- JavaFX/02 Subvistes/Exemple 0200/src/main/java/com/project/Controller.java
+++ JavaFX/02 Subvistes/Exemple 0200/src/main/java/com/project/Controller.java
@@ -1,14 +1,11 @@
 package com.project;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
